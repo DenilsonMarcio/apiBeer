c0c3d9adcbfecd8ba322e84e29512340e3d9c6b4
Denilson Marcio <moppahtech@gmail.com>
2020-12-14 04:41:15 -0300

    add frontend project

---
+++ Api_beer/src/main/java/one/digitalinnovation/beerstock/dto/QuantityDTO.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.beerstock.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuantityDTO {
+
+    @NotNull
+    @Max(100)
+    private Integer quantity;
+
+    /* TO DO Veriicar esse metodo !!! */
+    // public int getQuantity() {
+    // return quantity;
+    // }
+}
---
+++ Api_beer/src/main/java/one/digitalinnovation/beerstock/enums/BeerType.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.beerstock.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BeerType {
+
+    LAGER("Lager"), MALZBIER("Malzbier"), WITBIER("Witbier"), WEISS("Weiss"), ALE("Ale"), IPA("IPA"), APA("APA"),
+    STOUT("Stout");
+
+    private String description;
+    // private BeerType(String description) {
+    // }
+}
